73f58ba14c63e076ca17207e6805902fc834402f
cg <chuganghong@qq.com>
2020-10-13 22:18:45 +0800

    还原画板JS代码

--- webrtc-main/src/utils/palette.js
+++ webrtc-main/src/utils/palette.js
@@ -2,7 +2,7 @@
  * Created by wyw on 2019/3/28.
  */
 class Palette {
-    constructor(canvas, {drawType = 'line', paint, imgDataInit, inputElement, drawColor = 'rgba(19, 206, 102, 1)', lineWidth = 5, sides = 3, allowCallback, moveCallback}) {
+    constructor(canvas, {drawType = 'line', paint, imgDataInit, drawColor = 'rgba(19, 206, 102, 1)', lineWidth = 5, sides = 3, allowCallback, moveCallback}) {
         this.canvas = canvas;
         this.width = canvas.width; // 宽
         this.height = canvas.height; // 高
@@ -19,59 +19,14 @@ class Palette {
         this.drawColor = drawColor; // 绘制颜色
         this.lineWidth = lineWidth; // 线条宽度
         this.sides = sides; // 多边形边数
-
-        this.inputElement = inputElement;
-        // this.rectTxt = {
-        //     x: 20,
-        //     y: 20,
-        //     width: 150,
-        //     height: 50
-        // };
-        this.rectTxt = {
-            x: 180,
-            y: 200,
-            width: 150,
-            height: 50
-        };
-        this.mouseDownTxt = false;
-        this.deltaX = 0;
-        this.deltaY = 0;
-        this.toolText = 'hello';
-        this.textX = 0;
-        this.textY = 0;
-        //
-        // this.onkeyupCallback = function () {
-        //
-        // };
-
-        this.bindMouseKeyup = function () {
-        }
-        this.bindDbClick = function () {
-        }
-
-
-        this.allowCallback = allowCallback || function () {
-        }; // 允许操作的回调
-        this.moveCallback = moveCallback || function () {
-        }; // 鼠标移动的回调
-        this.bindMousemove = function () {
-        }; // 解决 eventlistener 不能bind
-        this.bindMousedown = function () {
-        }; // 解决 eventlistener 不能bind
-        this.bindMouseup = function () {
-        }; // 解决 eventlistener 不能bind
+        this.allowCallback = allowCallback || function () {}; // 允许操作的回调
+        this.moveCallback = moveCallback || function () {}; // 鼠标移动的回调
+        this.bindMousemove = function () {}; // 解决 eventlistener 不能bind
+        this.bindMousedown = function () {}; // 解决 eventlistener 不能bind
+        this.bindMouseup = function () {}; // 解决 eventlistener 不能bind
         this.init();
     }
-
     init() {
-        // 文字工具 start
-        const inputEle = this.inputElement;
-        // this.canvas.ondblclick = function (e) {
-        //     inputEle.setAttribute('style', `left:${e.clientX}px;top:${e.clientY}px;display:block`);
-        //     inputEle.focus();
-        // }
-        // 文字工具 end
-
         this.imgData.push(this.imgDataInit)
         this.paint.fillStyle = '#fff';
         this.paint.clearRect(0, 0, this.width, this.height);
@@ -89,28 +44,14 @@ class Palette {
         this.bindMouseup = this.onmouseup.bind(this);
         this.canvas.addEventListener('mousedown', this.bindMousedown);
         document.addEventListener('mouseup', this.bindMouseup);
-
-        this.bindMouseKeyup = this.onkeyup.bind(this);
-        this.inputElement.addEventListener('keyup', this.bindMouseKeyup);
-
-        this.bindDbClick = this.ondblclick.bind(this);
-        this.canvas.addEventListener('dblclick', this.bindDbClick);
     }
-
     onmousedown(e) { // 鼠标按下
         this.isClickCanvas = true;
         this.x = e.offsetX;
         this.y = e.offsetY;
         this.last = [this.x, this.y];
-
-        let rect = this.rectTxt;
-        this.deltaX = e.clientX - rect.x;
-        this.deltaY = e.clientY - rect.y;
-        this.mouseDownTxt = true;
-
         this.canvas.addEventListener('mousemove', this.bindMousemove);
     }
-
     gatherImage() { // 采集图像
         this.imgData = this.imgData.slice(0, this.index + 1); // 每次鼠标抬起时，将储存的imgdata截取至index处
         let imgData = this.paint.getImageData(0, 0, this.width, this.height);
@@ -121,20 +62,16 @@ class Palette {
         console.log('this.imgDataInit2 end')
         this.allowCallback(this.index > 0, this.index < this.imgData.length - 1);
     }
-
     reSetImage() { // 重置为上一帧
         this.paint.clearRect(0, 0, this.width, this.height);
-        if (this.imgData.length >= 1) {
+        if(this.imgData.length >= 1){
             this.paint.putImageData(this.imgData[this.index], 0, 0);
         }
     }
-
     onmousemove(e) { // 鼠标移动
         this.isMoveCanvas = true;
         let endx = e.offsetX;
         let endy = e.offsetY;
-        let clientX = e.clientX;
-        let clientY = e.clientY;
         let width = endx - this.x;
         let height = endy - this.y;
         let now = [endx, endy]; // 当前移动到的位置
@@ -168,15 +105,9 @@ class Palette {
                 this.moveCallback('eraser', ...params);
                 this.eraser(...params);
             }
-            case 'txt' : {
-                let params = [clientX, clientY, this.width, this.height, this.lineWidth];
-                this.moveCallback('txt', ...params);
-                this.txt(...params);
-            }
                 break;
         }
     }
-
     onmouseup() { // 鼠标抬起
         if (this.isClickCanvas) {
             this.isClickCanvas = false;
@@ -188,8 +119,11 @@ class Palette {
             }
         }
     }
-
     line(last, now, lineWidth, drawColor) { // 绘制线性
+        console.log('===============line start')
+        console.log(last)
+        console.log(now)
+        console.log('===============line end')
         this.paint.beginPath();
         this.paint.lineCap = "round"; // 设定线条与线条间接合处的样式
         this.paint.lineJoin = "round";
@@ -201,14 +135,12 @@ class Palette {
         this.paint.stroke(); // 进行绘制
         this.last = now;
     }
-
     rect(x, y, width, height, lineWidth, drawColor) { // 绘制矩形
         this.reSetImage();
         this.paint.lineWidth = lineWidth;
         this.paint.strokeStyle = drawColor;
         this.paint.strokeRect(x, y, width, height);
     }
-
     polygon(x, y, sides, width, height, lineWidth, drawColor) { // 绘制多边形
         this.reSetImage();
         let n = sides;
@@ -217,24 +149,22 @@ class Palette {
         this.paint.beginPath();
         this.paint.strokeStyle = drawColor;
         this.paint.lineWidth = lineWidth;
-        for (let i = 0; i < n; i++) {
+        for(let i=0; i < n; i++){
             this.paint.lineTo(x + Math.sin((i * ran + 45) * Math.PI / 180) * rn, y + Math.cos((i * ran + 45) * Math.PI / 180) * rn);
         }
         this.paint.closePath();
         this.paint.stroke();
     }
-
     arc(x, y, width, height, lineWidth, drawColor) { // 绘制圆形
         this.reSetImage();
         this.paint.beginPath();
         this.paint.lineWidth = lineWidth;
         let r = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-        this.paint.arc(x, y, r, 0, Math.PI * 2, false);
+        this.paint.arc(x, y, r, 0, Math.PI*2, false);
         this.paint.strokeStyle = drawColor;
         this.paint.closePath();
         this.paint.stroke();
     }
-
     eraser(endx, endy, width, height, lineWidth) { // 橡皮擦
         this.paint.save();
         this.paint.beginPath();
@@ -246,25 +176,22 @@ class Palette {
         this.paint.fillRect(0, 0, width, height);
         this.paint.restore();
     }
-
     cancel() { // 撤回
-        if (--this.index < 0) {
+        if (--this.index <0) {
             this.index = 0;
             return;
         }
         this.allowCallback(this.index > 0, this.index < this.imgData.length - 1);
         this.paint.putImageData(this.imgData[this.index], 0, 0);
     }
-
-    go() { // 前进
-        if (++this.index > this.imgData.length - 1) {
-            this.index = this.imgData.length - 1;
+    go () { // 前进
+        if (++this.index > this.imgData.length -1) {
+            this.index = this.imgData.length -1;
             return;
         }
         this.allowCallback(this.index > 0, this.index < this.imgData.length - 1);
         this.paint.putImageData(this.imgData[this.index], 0, 0);
     }
-
     clear() { // 清屏
         this.imgData = [];
         this.paint.clearRect(0, 0, this.width, this.height);
@@ -272,14 +199,12 @@ class Palette {
         this.paint.fillRect(0, 0, this.width, this.height);
         this.gatherImage();
     }
-
     changeWay({type, color, lineWidth, sides}) { // 绘制条件
         this.drawType = type !== 'color' && type || this.drawType; // 绘制形状
         this.drawColor = color || this.drawColor; // 绘制颜色
         this.lineWidth = lineWidth || this.lineWidth; // 线宽
         this.sides = sides || this.sides; // 边数
     }
-
     destroy() {
         this.clear();
         this.canvas.removeEventListener('mousedown', this.bindMousedown);
@@ -287,89 +212,7 @@ class Palette {
         this.canvas = null;
         this.paint = null;
     }
-
-    txt(endx, endy) { // 文字
-        let deltaX = this.deltaX;
-        let deltaY = this.deltaY;
-        let rect = this.rectTxt
-
-        const judgeW = this.width - rect.width, judgeH = this.height - rect.height;
-
-        if (this.mouseDownTxt) {
-            /** 相减即可获得矩形左边界与canvas左边界之间的长度 */
-            let dx = endx - deltaX;
-            let dy = endy - deltaY;
-            if (dx < 0) {
-                dx = 0;
-            } else if (dx > judgeW) {
-                dx = judgeW;
-            }
-            if (dy < 0) {
-                dy = 0;
-            } else if (dy > judgeH) {
-                dy = judgeH;
-            }
-            rect.x = dx;
-            rect.y = dy;
-            this.txtStrokeRect()
-        }
-    }
-
-    /** 清除指定区域 */
-    txtClearRect() {
-        this.paint.clearRect(0, 0, this.width, this.height)
-    }
-
-    /** 画矩形 */
-    txtStrokeRect(e) {
-        /** 设置文字和边框样式 */
-        this.paint.font = "16px Arial";
-        this.paint.fillStyle = "#fff";
-        /** 设置为 center 时，文字段的中心会在 fillText的 x 点 */
-        this.paint.textAlign = 'center';
-        this.paint.lineWidth = '2';
-        this.paint.strokeStyle = '#fff';
-
-        let text = this.toolText
-        let rect = this.rectTxt
-
-        this.reSetImage();
-        // this.txtClearRect()
-
-        /** 这里如果不调用 beginPath 历史的矩形会重新被绘制 */
-        this.paint.beginPath()
-
-        let clientX = this.textX
-        let clientY = this.textY
-        this.paint.rect(clientX, clientY, rect.width, rect.height)
-        this.paint.stroke();
-        // 设置字体内容，以及在画布上的位置
-        this.paint.fillText(text, clientX + 70, clientY + 30);
-    }
-
-    onkeyup(e) {
-        const inputEle = this.inputElement;
-        if (e.keyCode === 13) {
-            this.toolText = inputEle.value;
-            this.txtStrokeRect(e)
-            inputEle.setAttribute('style', `display:none`)
-        }
-    }
-
-
-    ondblclick(e) {
-        this.textX = e.clientX;
-        this.textY = e.clientY;
-
-        let inputEle = this.inputElement;
-        inputEle.setAttribute('style', `left:${e.clientX - 100}px;top:${e.clientY - 100}px;display:block`);
-        inputEle.focus();
-        //
-        // alert(this.textX);
-        // alert(this.textY);
-    }
 }
-
 export {
     Palette
 }
