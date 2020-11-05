35acc35f7860684e41a81df0f07273b607669e4c
cg <chuganghong@qq.com>
2020-11-06 03:45:53 +0800

    add  main/src/components/common/

---
+++ main/src/components/common/Global.vue
@@ -0,0 +1,14 @@
+<script>
+const serverSrc = 'www.baidu.com';
+const apiHost = process.env.NODE_ENV == 'development' ? 'http://localhost:4000' : 'http://meeting-api.chugang.net';
+const token = '12345678';
+const hasEnter = false;
+const userSite = '中国钓鱼岛';
+export default {
+  userSite,//用户地址
+  token,//用户token身份
+  serverSrc,//服务器地址
+  apiHost,
+  hasEnter,//用户登录状态
+};
+</script>
