c9a4954860209126ee4d7b02fabe1894d234da35
cg <chuganghong@qq.com>
2020-11-06 04:19:05 +0800

    修正router

--- main/src/main.js
+++ main/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router/router';
+import router from './router';
 // import store from './store/store';
 import socket from './utils/socket';
 import bus from './utils/eventBus';
