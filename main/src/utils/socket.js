30f31c0112ae52d685f4c7d00c2836f7588edd57
cg <chuganghong@qq.com>
2020-11-07 07:05:48 +0800

    修改本站地址http为https，调试

--- main/src/utils/socket.js
+++ main/src/utils/socket.js
@@ -1,6 +1,6 @@
 
 import io from 'socket.io-client';
-// let host = location.origin;
-let host = 'https://meeting.chugang.net:3001'
+let host = location.origin;
+// let host = 'https://meeting.chugang.net:3001'
 const socket = io.connect(host);
 export default socket;
