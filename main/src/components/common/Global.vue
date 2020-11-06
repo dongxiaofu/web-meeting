c3b7f7d2c8b942f58c18417d013f40b01ff04773
cg <chuganghong@qq.com>
2020-11-06 16:38:42 +0800

    增加本网站地址

--- main/src/components/common/Global.vue
+++ main/src/components/common/Global.vue
@@ -1,6 +1,7 @@
 <script>
 const serverSrc = 'www.baidu.com';
 const apiHost = process.env.NODE_ENV == 'development' ? 'http://localhost:4000' : 'http://meeting-api.chugang.net';
+const webHost = process.env.NODE_ENV == 'development' ? 'http://127.0.0.1:3221' : 'http://meeting.chugang.net';
 const token = '12345678';
 const hasEnter = false;
 const userSite = '中国钓鱼岛';
@@ -8,7 +9,8 @@ export default {
   userSite,//用户地址
   token,//用户token身份
   serverSrc,//服务器地址
-  apiHost,
+  apiHost,  // api地址
+  webHost,  // 本网站地址
   hasEnter,//用户登录状态
 };
 </script>
